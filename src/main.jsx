f0e597c29d21290b34e31cefdde57a94dd0b7f8a
Alec Robson <alecrobsona@Alecs-MacBook-Pro.local>
2026-02-24 21:15:07 -0800

    lost

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import FlowScaffoldApp from './FlowScaffoldApp.jsx'
-
-const useFlowScaffold = import.meta.env.VITE_ENABLE_FLOW_SCAFFOLD === '1'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {useFlowScaffold ? <FlowScaffoldApp /> : <App />}
+    <App />
   </StrictMode>,
 )
